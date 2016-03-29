bd4670f2ec31b510abaa86c050c730b322d1b6f0
Anna Kruglik <kruglik.anna@gmail.com>
2016-03-29 13:44:34 +0800

    removed code creating and starting the rate dialog from the main lib class

--- library/src/main/java/trikita/promote/Promote.java
+++ library/src/main/java/trikita/promote/Promote.java
@@ -1,12 +1,8 @@
 package trikita.promote;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 
 public final class Promote {
 
@@ -154,24 +150,5 @@ public final class Promote {
 	}
     }
 
-    public static Dialog rateDialog(final Context c, final int id) {
-	return new AlertDialog.Builder(c)
-	    .setTitle(R.string.rate_dialog_title)
-	    .setMessage(R.string.rate_dialog_message)
-	    .setPositiveButton(R.string.rate_dialog_btn_ok, new DialogInterface.OnClickListener() {
-		public void onClick(DialogInterface dialog, int which) {
-		    c.startActivity(new Intent(Intent.ACTION_VIEW,
-			    Uri.parse("https://play.google.com/store/apps/details?id=" + c.getPackageName())));
-		    Promote.ban(c, id);
-		}
-	    }).setNegativeButton(R.string.rate_dialog_btn_cancel, new DialogInterface.OnClickListener() {
-		public void onClick(DialogInterface dialog, int which) {
-		    Promote.ban(c, id);
-		}
-	    }).setNeutralButton(R.string.rate_dialog_btn_later, new DialogInterface.OnClickListener() {
-		public void onClick(DialogInterface dialog, int which) {
-		    dialog.dismiss();
-		}
-	    }).create();
     }
 }
